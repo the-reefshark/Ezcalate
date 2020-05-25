5c63b61a3ae47fce577dcaa403122514f80edff5
rishabhpaliwal <rishabh92k@gmail.com>
2020-05-25 19:34:25 +0800

    Added add box

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,18 +7,34 @@ class TodoList extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: todosData
+            todos: todosData,
+            add: ""
         }
+    }
+
+    handleAdd = event => {
+        event.preventDefault();
+        const newTodo = {
+            id: this.state.todos.length + 1,
+            text: this.state.add,
+            completed: false
+        }
+        const updatedTodos = [...this.state.todos, newTodo]
+        this.setState({ todos: updatedTodos, add: "" })
+    }
+
+    handleAddBox = event => {
+        this.setState({add: event.target.value})
     }
 
     handleChange = id => {
         const updatedTodos = this.state.todos.map(todo => {
           if (todo.id === id) {
-            todo.completed = !todo.completed;
+            todo.completed = !todo.completed
           }
-          return todo;
+          return todo
         })
-        this.setState({ todos: updatedTodos });
+        this.setState({ todos: updatedTodos })
     }
 
     handleClick = id => {
@@ -32,6 +48,16 @@ class TodoList extends React.Component {
             handleChange={this.handleChange} handleClick={this.handleClick} />)
         return (
             <div className="todo-list">
+                <form onSubmit={this.handleAdd}>
+                    <input
+                        type="text"
+                        name="newTodo"
+                        value={this.state.add}
+                        onChange={this.handleAddBox}
+                    />
+                    <button> Add </button>
+                    <br />
+                </form>
                 {todoItems}
             </div>
         )
